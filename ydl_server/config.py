aea1b7da21a1424e7c2c2f28c14e1768548d8b7f
Nick Vance <nickv2002@users.noreply.github.com>
2021-05-06 14:00:02 -0700

    dont allow av01 (av1) video downloads

--- ydl_server/config.py
+++ ydl_server/config.py
@@ -1,5 +1,5 @@
 app_defaults = {
-    'YDL_FORMAT': 'bestvideo+bestaudio/best',
+    'YDL_FORMAT': 'bestvideo[vcodec!^=av01]+bestaudio/best',
     'YDL_EXTRACT_AUDIO_FORMAT': None,
     'YDL_EXTRACT_AUDIO_QUALITY': '192',
     'YDL_RECODE_VIDEO_FORMAT': None,
