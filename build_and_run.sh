3f16f4a8a7ae9fdc1b750954102d7ce52986c04f
Nick Vance <nickv2002@users.noreply.github.com>
2021-08-11 15:55:01 -0700

    multi arch build fixes, to push to local or dockerhub

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,12 +5,17 @@ set -x
 # stop exiting docker containers
 docker kill youtube-dl
 docker rm youtube-dl
-rm ./ytdl-test/*
-rm ./ytdl-test/.*
+rm -rf ./ytdl-test/*
+rm -rf ./ytdl-test/.*
 
-docker build -t registry.local:5000/twl-dl-server -t twl-dl-server -t towatchlist/twl-dl-server .
-docker push registry.local:5000/twl-dl-server
-# docker push towatchlist/twl-dl-server
+# multi arch build and push to local registry
+docker buildx build \
+  --output=type=registry,registry.insecure=true \
+  --platform linux/arm64/v8,linux/amd64 \
+  -t registry.local:5000/twl-dl-server \
+  .
+
+docker pull registry.local:5000/twl-dl-server
 
 # localPath='/Volumes/Video/Other/ToWatchList'
 localPath='/Users/nick/Documents/ToWatchList/twl-dl-server/ytdl-test'
@@ -22,10 +27,19 @@ docker run -d --name youtube-dl \
   --env TWL_LOOKBACK_TIME_STRING=-3min \
   --env YDL_WRITE_NFO=True \
   --env KODI_URL='http://10.0.1.54:8080/jsonrpc' \
-  twl-dl-server
+  registry.local:5000/twl-dl-server
 docker ps
 sleep 2
 
 curl "http://localhost:8080/api/twl/update?TWL_LOOKBACK_TIME_STRING=-1days"
 # curl "http://uzfs.local:8085/api/twl/update"
 open "http://localhost:8080/logs"
+
+exit
+
+# push to DockerHub registry
+docker buildx build \
+  --push \
+  --platform linux/arm64/v8,linux/amd64 \
+  -t towatchlist/twl-dl-server \
+  .
