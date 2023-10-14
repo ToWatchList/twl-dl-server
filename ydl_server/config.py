299419c25978c3e3ae6f1b77038005b83792cc49
Nick Vance <nick@towatchlist.com>
2023-10-14 12:48:58 -0700

    allow AV1 video format

--- ydl_server/config.py
+++ ydl_server/config.py
@@ -1,5 +1,5 @@
 app_defaults = {
-    "YDL_FORMAT": "bestvideo[vcodec!^=av01]+bestaudio/best",
+    "YDL_FORMAT": "bestvideo+bestaudio/best",
     "YDL_EXTRACT_AUDIO_FORMAT": None,
     "YDL_EXTRACT_AUDIO_QUALITY": None,
     "YDL_RECODE_VIDEO_FORMAT": None,
